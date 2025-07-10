b625a279ab860b80e42394065ac782203d4ab125
Caelan Ross <caelan.ross@gmail.com>
2025-07-09 19:02:04 -0600

    Upgrade navbar to MUI and make it sticky

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,30 +1,73 @@
-import { Outlet, NavLink } from "react-router-dom";
-import '../styles/navbar.css';
-import logo from '../images/logo-no-background.png'
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import { NavLink, Outlet } from "react-router-dom";
+import logo from "../images/logo-no-background.png";
+
+const StyledNavLink = styled(NavLink)(({ theme }) => ({
+  textDecoration: "none",
+  fontSize: 16,
+  fontWeight: 400,
+  color: "#ccc9d3",
+  position: "relative",
+  "&.active": {
+    color: "#d0bbbb",
+    fontWeight: 500,
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      bottom: -4,
+      left: 0,
+      width: "100%",
+      height: 2,
+      backgroundColor: "#d0bbbb",
+    },
+  },
+}));
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar">
-      <div className="container">
-        <div class="logo-image">
-          <img id="profile-image" src={logo} alt="logo with words Caelan Ross." class="img-fluid"/>
-        </div>
-        <div className="nav-elements">
-            <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <AppBar
+        position="fixed"
+        elevation={0}
+        sx={{
+          backgroundColor: "#303039",
+          height: "60px",
+          justifyContent: "center",
+        }}
+      >
+        <Container
+          maxWidth="lg"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            component="img"
+            src={logo}
+            alt="logo with words Caelan Ross."
+            sx={{
+              maxHeight: 50,
+              maxWidth: 200,
+              mt: "-6px",
+            }}
+          />
+          <Stack direction="row" spacing={6}>
+            <StyledNavLink to="/">Home</StyledNavLink>
+            <StyledNavLink to="/contact">Contact</StyledNavLink>
+          </Stack>
+        </Container>
+      </AppBar>
+      <Toolbar sx={{ minHeight: "60px" }} />
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Navbar;
