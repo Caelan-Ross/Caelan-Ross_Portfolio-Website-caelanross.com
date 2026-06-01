c51247e5a738ee20b664116f9a8c6aa1efe06071
Caelan Ross <41312605+Caelan-Ross@users.noreply.github.com>
2026-06-01 15:00:29 -0600

    Fix linter error

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Chip from "@mui/material/Chip";
 import Collapse from "@mui/material/Collapse";
